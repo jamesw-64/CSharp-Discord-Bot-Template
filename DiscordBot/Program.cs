3d8ebb2fcb707643d1f181afe1da2997b8ec46d6
James Wright <jamesw@kainos.com>
2019-10-09 23:11:13 +0100

    Initial commit

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO Create init menu to input tokens and store the output locally
+            //     to prevent token leaking
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
