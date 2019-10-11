08dcedafeb04f447b045a06ff646bc488cf587de
James Wright <jamesw@kainos.com>
2019-10-11 10:22:11 +0100

    Removed DeepAI and created template for a C# discord bot

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,85 +1,127 @@
-﻿using System;
+﻿//using list
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
-using DeepAI;
 using Discord.WebSocket;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
+//File written (mostly) by James Wright. See the repo here: https://github.com/jamesw-64/DiscordBot
 
 namespace DiscordBot
 {
     class Program
     {
+        //With some help from https://discord.foxbot.me/docs/guides/getting_started/first-bot.html
+
         //create global variables 
         private DiscordSocketClient _client;
 
-        System.IO.StreamReader tokenInput;
+        StreamReader tokenInput;
         string tokenLoop;
         string token;
         static string startTime = DateTime.Now.ToString("HH-mm-ss");
 
-        DeepAI_API api = new DeepAI_API(apiKey: "nope");
-
+        //call main but redirect to MainAsync
         public static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
+            //Prepare crash handler to save crash log
             AppDomain currentDomain = AppDomain.CurrentDomain;
             currentDomain.UnhandledException += new UnhandledExceptionEventHandler(CrashHandler);
 
-            Log("\n _              _       \n| \\o _ _ _ .__||_) __|_ \n|_/|_>(_(_)|(_||_)(_)|_ \n");
-
-            try
+            //Print logo (remove if you want)
+            await Log("\n _              _       \n| \\o _ _ _ .__||_) __|_ \n|_/|_>(_(_)|(_||_)(_)|_ \n");
+            
+            try //to read in token
             {
-                tokenInput = new System.IO.StreamReader(@"token.txt");
-            } catch (System.IO.FileNotFoundException)
+                tokenInput = new StreamReader(@"token.txt");
+            } catch (FileNotFoundException) //If we cannot find the token.txt file...
             {
+                //Tell the user that they need to input a token
                 Log("We could not find your token.txt file. This is needed to communicate with your bot. Would you like to enter one now? (Y/N) ", false);
+
+                //read input
                 String input = Console.ReadLine();
+
+                //standarise input (make it capital)
                 input = input.ToUpper();
+
+                //switch case to check input
                 switch (input){
                     case "Y":
+                        //get the token input
                         Log("Please input your token and press enter: ", false);
                         token = Console.ReadLine();
 
-                        Log("Saving input...");
-                        System.IO.File.WriteAllText(@"token.txt", token);
-                        Log("token.txt saved to " + System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\token.txt");
+                        //save the input
+                        await Log("Saving input...");
+                        File.WriteAllText(@"token.txt", token);
+                        await Log("token.txt saved to " + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\token.txt");
 
-                        Log("Thank you. The program will now restart... (STOPCODE 3)");
-                        Process.Start(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\DiscordBot.exe");
+                        //restart program
+                        await Log("Thank you. The program will now restart... (STOPCODE 3)");
+                        Process.Start(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\DiscordBot.exe");
                         Environment.Exit(3);
                         break;
                     case "N":
-                        Log("Program cannont operate without bot token. Exitiing... (STOPCODE 1)");
+                        //end the program as we cannot contiune without token
+                        await Log("Program cannont operate without bot token. Exitiing... (STOPCODE 1)");
                         Environment.Exit(1);
                         break;
                     default:
-                        Log("Did not understand input. Exiting... (STOPCODE 2)");
+                        //what was that input? we cannout continue without token
+                        await Log("Did not understand input. Exiting... (STOPCODE 2)");
                         Environment.Exit(2);
                         break;
                 } 
             }
 
+            //clean up token as some OS's add a newline to the bottom of a file,
+            //which can crash the program.
             while ((tokenLoop = tokenInput.ReadLine()) != null)
             {
                 token = tokenLoop;
             }
 
+            //close the file containing the token
             tokenInput.Close();
 
+            /*
+             *
+             * IF YOU DO NOT WANT TO USE A FILE TO READ IN THE TOKEN, DO THE FOLLOWING:
+             *
+             *  - REMOVE StreamReader tokenInput; on line 18
+             *  - REMOVE string tokenLoop; on line 19
+             *  - REMOVE lines 36 - 73
+             *
+             *  - ADD token = "<insert token here>";
+             *
+             *  However, if you decide to hardcode your token into the program  please
+             *  remember to not push this to a public repo as doing so will expose
+             *  your bot token, allowing anyone to control your bot. Consider making
+             *  methods that are async Tasks so that log running commands don't freeze
+             *  up the bot.
+             *  
+             */
+
+            //print token to screen but don't log it as doing so may expose token
             Console.WriteLine("Welcome! Token is: " + token + " (this line is not logged, so don't worry about submitting the log file if there is a crash)");
+
+            //create a new socketclient
             _client = new DiscordSocketClient();
 
-            _client.Log += LogMsg;
+            //what to do when the _client needs to output
+            _client.Log += Log;
 
+            //Log in using token
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
+            //what to to when a message is recieved (watches all channel in the server it's in)
             _client.MessageReceived += MessageReceived;
 
             // Block this task until the program is closed.
@@ -88,68 +130,45 @@ namespace DiscordBot
 
         private async Task MessageReceived(SocketMessage message)
         {
-            Log("Message Received! Contents: '" + message.Content + "' from " + message.Source + ": " + message.Author + " in channel: #" + message.Channel);
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
-            if (message.Content.StartsWith("!generate"))
-            {
-                string msg = (message.Content).Replace("!generate ", "");
-                Log("Sending: '" + msg + "' to DeepAI");
+            //output to console the message that was received
+            //IDEA FOR IMPORVEMENT: LOG MESSAGES TO A DATABASE AND NOT A FILE
+            await Log("Message Received! Contents: '" + message.Content + "' from " + message.Source + ": " + message.Author + " in channel: #" + message.Channel);
 
-                await message.Channel.SendMessageAsync("Sending text to DeepAI and generating text, this may take a while and the bot will be unresponsive (maybe?) until task is completed. Please remember that the result of the generation does not reflect the views of any real people...");
+            /*
+             *
+             * This is where you program your main functionality of your program.
+             * You can use if/else statments or switch/case stamtements to program
+             * in commands.
+             *
+             */
 
-                await SendToAI(msg, message);
-            }
-            if (message.Content == "!end" && (message.Author).ToString == "GhostCode#6938")
+            switch (message.Content)
             {
-                await message.Channel.SendMessageAsync("Good night!");
-                Environment.Exit(0);
+                //an example of a command
+                case "!ping":
+                    await message.Channel.SendMessageAsync("Pong!");
+                    break;
             }
         }
 
-        private Task LogMsg(LogMessage msg)
+        //Logging for _client
+        private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             File.AppendAllText(@"DiscordBot_log_" + startTime + ".log", msg.ToString() + Environment.NewLine);
             return Task.CompletedTask;
         }
 
-        private Task SendToAI(string msg, SocketMessage message)
-        {
-            StandardApiResponse resp = api.callStandardApi("text-generator", new
-            {
-                text = msg,
-            });
-
-            var data = (JObject)JsonConvert.DeserializeObject(api.objectAsJsonString(resp));
-
-            System.IO.File.WriteAllText(@"deepai_output.txt", data["output"].Value<string>());
-
-            message.Channel.SendFileAsync(@"deepai_output.txt");
-
-            File.Delete(@"deepai_output.txt");
-
-            return Task.CompletedTask;
-        }
-
-        static void CrashHandler(object sender, UnhandledExceptionEventArgs args)
-        {
-            Exception e = (Exception)args.ExceptionObject;
-            string msg = DateTime.Now.ToString("HH:mm:ss") + ": Oops, the program has crash without being caught. Crash handler caught: " + e + "\nRuntime terminating? " + args.IsTerminating;
-            Console.WriteLine();
-            File.AppendAllText(@"DiscordBot_log_" + startTime + ".log", msg + Environment.NewLine);
-            Environment.Exit(4);
-        }
-
-        private void Log(string msg)
+        //general logging
+        private Task Log(string msg)
         {
             msg = DateTime.Now.ToString("HH:mm:ss") + ": " + msg;
             File.AppendAllText(@"DiscordBot_log_" + startTime + ".log", msg + Environment.NewLine);
             Console.WriteLine(msg);
+            return Task.CompletedTask;
         }
 
+        //Logging, but with the option to use a WriteLine or Write statement
         private void Log(string msg, Boolean WriteLine)
         {
             msg = DateTime.Now.ToString("HH:mm:ss") + ": " + msg;
@@ -163,6 +182,16 @@ namespace DiscordBot
                     Console.Write(msg);
                     break;
             }
+        }
+
+        //crash handler that will save Stack trace on unhandled execption
+        static void CrashHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            string msg = DateTime.Now.ToString("HH:mm:ss") + ": Oops, the program has crash without being caught. Crash handler caught: " + e + "\nRuntime terminating? " + args.IsTerminating;
+            Console.WriteLine();
+            File.AppendAllText(@"DiscordBot_log_" + startTime + ".log", msg + Environment.NewLine);
+            Environment.Exit(4);
         }
     } // end class
 } //end namespace
